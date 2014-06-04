ebf56f42674a4fe0000182143227cdeefe92c6f9
Rick Quantz <rquantz@gmail.com>
2014-06-03 23:55:13 -0400

    Spec dkSwipeup

---
+++ test/directives/dkSwipeSpec.js
@@ -0,0 +1,19 @@
+describe('desktopSwipe', function() {
+  var element;
+
+  beforeEach(module('desktopSwipe'));
+
+  describe('dkSwipeup', function() {
+
+    it('should execute the callback on dk-swipeup', inject(function($rootScope, $compile) {
+      element = $compile('<div dk-swipeup="swiped = true"></div>')($rootScope);
+      $rootScope.$digest();
+
+      expect($rootScope.swiped).toBeUndefined();
+
+      element.trigger('dk-swipeup');
+      expect($rootScope.swiped).toBe(true);
+    }));
+
+  });
+});
