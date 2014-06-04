03eb2a5c26a59ca4a741ec95d18768c390e99dde
Rick Quantz <rquantz@gmail.com>
2014-06-04 09:12:05 -0400

    Implement dkSwipeup

--- test/directives/dkSwipeSpec.js
+++ test/directives/dkSwipeSpec.js
@@ -5,13 +5,13 @@ describe('desktopSwipe', function() {
 
   describe('dkSwipeup', function() {
 
-    it('should execute the callback on dk-swipeup', inject(function($rootScope, $compile) {
+    it('should execute the callback on dk.swipeup', inject(function($rootScope, $compile) {
       element = $compile('<div dk-swipeup="swiped = true"></div>')($rootScope);
       $rootScope.$digest();
 
       expect($rootScope.swiped).toBeUndefined();
 
-      element.trigger('dk-swipeup');
+      element.triggerHandler('dk.swipeup');
       expect($rootScope.swiped).toBe(true);
     }));
 
