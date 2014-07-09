aae76f22af586ef44701600b33d80a7c9b85b8e6
Rick Quantz <rquantz@gmail.com>
2014-07-09 18:27:45 -0400

    Fixture generator captures mousewheel events and outputs them in a way that is useful for testing swipe input

---
+++ test/tools/fixtureGenerator/fixtureGenerator.js
@@ -0,0 +1,57 @@
+$(function() {
+
+  var output = [], current;
+
+  $('.start').on('click', function() {
+    output.push([]);
+    current = output.length - 1;
+  });
+
+  $('.stop').on('click', function() {
+    current = null;
+    renderOutput();
+  });
+
+
+  $(window).on('mousewheel', function(event, delta) {
+    if (current == null) { return true; }
+
+    output[current].push([event, delta]);
+
+    renderOutput();
+    return false;
+  });
+
+
+
+  function renderOutput() {
+    var outputString = "\nvar output = [\n";
+
+    for (var i = 0; i < output.length; i++) {
+      outputString += "  [\n";
+
+      for (var j = 0; j < output[i].length; j++) {
+        console.log(output[i][j])
+        outputString += "    [";
+        outputString += "{";
+        outputString += "deltaX: " + output[i][j][0].deltaX;
+        outputString += ", ";
+        outputString += "deltaY: " + output[i][j][0].deltaY;
+        outputString += "}, ";
+        outputString += output[i][j][1];
+        outputString += "],\n";
+      }
+
+      if (i == output.length - 1) {
+        outputString += "  ]\n";
+      }
+      else {
+        outputString += "  ],\n";
+      }
+    }
+    outputString += "];";
+
+    $('.output').html(outputString);
+  }
+
+});
